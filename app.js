88e788de820de9b533d8f1265a8805ee5a02f8d5
fmikael <fadi_mikael@live.com>
2018-04-16 13:45:52 -0400

    merged from feature branch

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var passport = require('passport');
+var passport = requiregit('passport');
 var session = require('express-session');
 
 var index = require('./routes/index');
